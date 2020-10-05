7e2f2dd364f0cb55e63b11197113acf0b64e4a93
Peter Keefe <16809252+percula@users.noreply.github.com>
2020-10-05 13:37:48 -0400

    fix: Only update LD on main thread

--- preferenceutilslibrary/src/main/java/com/perculacreative/preferenceutils/library/SharedPreferencesLiveData.kt
+++ preferenceutilslibrary/src/main/java/com/perculacreative/preferenceutils/library/SharedPreferencesLiveData.kt
@@ -1,6 +1,8 @@
 package com.perculacreative.preferenceutils.library
 
 import android.content.SharedPreferences
+import android.os.Handler
+import android.os.Looper
 import androidx.lifecycle.MutableLiveData
 
 /**
@@ -17,7 +19,13 @@ abstract class SharedPreferenceLiveData<T>(val sharedPrefs: SharedPreferences,
 
     private val preferenceChangeListener = SharedPreferences.OnSharedPreferenceChangeListener { sharedPreferences, key ->
         if (key == this.key) {
-            getValueFromPreferences(key, defaultValue)?.takeIf { it != value }?.let { super.setValue(it) }
+            getValueFromPreferences(key, defaultValue)?.takeIf { it != value }?.let {
+                if (Looper.myLooper() != Looper.getMainLooper()) {
+                    Handler(Looper.getMainLooper()).post { super.setValue(it) }
+                } else {
+                    super.setValue(it)
+                }
+            }
         }
     }
 
@@ -66,9 +74,9 @@ class SharedPreferenceIntLiveData(sharedPrefs: SharedPreferences, key: String, d
 }
 
 class SharedPreferenceStringLiveData(sharedPrefs: SharedPreferences, key: String, defValue: String) :
-        SharedPreferenceLiveData<String?>(sharedPrefs, key, defValue) {
-    override fun getValueFromPreferences(key: String, defValue: String?): String? = sharedPrefs.getString(key, defValue)
-    override fun setValueInPreferences(value: String?) {
+        SharedPreferenceLiveData<String>(sharedPrefs, key, defValue) {
+    override fun getValueFromPreferences(key: String, defValue: String): String = sharedPrefs.getString(key, defValue) ?: defValue
+    override fun setValueInPreferences(value: String) {
         sharedPrefs.edit().putString(key, value).apply()
     }
 }
@@ -98,9 +106,9 @@ class SharedPreferenceLongLiveData(sharedPrefs: SharedPreferences, key: String, 
 }
 
 class SharedPreferenceStringSetLiveData(sharedPrefs: SharedPreferences, key: String, defValue: Set<String>) :
-        SharedPreferenceLiveData<Set<String>?>(sharedPrefs, key, defValue) {
-    override fun getValueFromPreferences(key: String, defValue: Set<String>?): Set<String>? = sharedPrefs.getStringSet(key, defValue)
-    override fun setValueInPreferences(value: Set<String>?) {
+        SharedPreferenceLiveData<Set<String>>(sharedPrefs, key, defValue) {
+    override fun getValueFromPreferences(key: String, defValue: Set<String>): Set<String> = sharedPrefs.getStringSet(key, defValue) ?: defValue
+    override fun setValueInPreferences(value: Set<String>) {
         sharedPrefs.edit().putStringSet(key, value).apply()
     }
 }
@@ -108,7 +116,7 @@ class SharedPreferenceStringSetLiveData(sharedPrefs: SharedPreferences, key: Str
 fun SharedPreferences.intLiveData(key: String, defValue: Int): SharedPreferenceLiveData<Int> {
     return SharedPreferenceIntLiveData(this, key, defValue)
 }
-fun SharedPreferences.stringLiveData(key: String, defValue: String): SharedPreferenceLiveData<String?> {
+fun SharedPreferences.stringLiveData(key: String, defValue: String): SharedPreferenceLiveData<String> {
     return SharedPreferenceStringLiveData(this, key, defValue)
 }
 fun SharedPreferences.booleanLiveData(key: String, defValue: Boolean): SharedPreferenceLiveData<Boolean> {
@@ -120,6 +128,6 @@ fun SharedPreferences.floatLiveData(key: String, defValue: Float): SharedPrefere
 fun SharedPreferences.longLiveData(key: String, defValue: Long): SharedPreferenceLiveData<Long> {
     return SharedPreferenceLongLiveData(this, key, defValue)
 }
-fun SharedPreferences.stringSetLiveData(key: String, defValue: Set<String>): SharedPreferenceLiveData<Set<String>?> {
+fun SharedPreferences.stringSetLiveData(key: String, defValue: Set<String>): SharedPreferenceLiveData<Set<String>> {
     return SharedPreferenceStringSetLiveData(this, key, defValue)
 }
